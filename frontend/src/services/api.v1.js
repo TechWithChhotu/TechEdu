8b84ecb416646a3775bdf015572b3c778e91b365
Chhotu Patel <chhotustudymail@gmail.com>
2026-01-13 11:36:50 +0530

    some changes

--- frontend/src/services/api.v1.js
+++ frontend/src/services/api.v1.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://techedu-wb83.onrender.com/api/v1",
   withCredentials: true, // IMPORTANT for cookies
   headers: {
     "Content-Type": "application/json",
